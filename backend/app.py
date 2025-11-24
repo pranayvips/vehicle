592ae7ea66b033b790f95b842f8fa2eb9c5d55da
Pranay <148135405+pranayvips@users.noreply.github.com>
2025-11-24 13:20:25 +0530

    Implement vehicle classification and threading in video
    Added vehicle classification and counting functionality using OpenCV. Implemented threading for video processing and updated the predictVideo route to handle video classification.

--- backend/app.py
+++ backend/app.py
@@ -9,6 +9,12 @@ import cv2
 import easyocr
 from flask_cors import CORS
 from flask_socketio import SocketIO, emit
+import cv2
+import numpy as np
+import time
+import vehicle
+from threading import Thread
+
 os.environ["KMP_DUPLICATE_LIB_OK"]= 'TRUE'
 
 def readVideo(filepath):  
@@ -62,7 +68,7 @@ def readVideo(filepath):
 
 
         for (i,c) in enumerate(counterShape):
-            (x,y,w,h)=cv2.boundingRect(c);
+            (x,y,w,h)=cv2.boundingRect(c)
             validate_counter=(w>=min_width_Rect) and (h>=min_height_Rect)
             if not validate_counter:
                 continue
@@ -94,6 +100,178 @@ def readVideo(filepath):
     cv2.destroyAllWindows()
     cap.release()
     out.release()
+
+def classify(filepath):
+
+    #---------------------------------------------------VARIABLE DECLARATIONS-------------------------------------------------------------------------------
+    cap=cv2.VideoCapture(filepath) 
+    fgbg=cv2.createBackgroundSubtractorMOG2(detectShadows=False,history=200,varThreshold = 90)
+    kernalOp = np.ones((3,3),np.uint8)
+    kernalOp2 = np.ones((5,5),np.uint8)
+    kernalCl = np.ones((11,11),np.uint8)
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cars = []
+    max_p_age = 5
+    pid = 1
+    cnt_up=0
+    cnt_down=0
+    line_up=400
+    line_down=250
+    up_limit=230
+    down_limit=int(4.5*(500/5))
+
+
+    carCount = 0
+    truckCount = 0
+
+
+
+    # Get the width, height, and FPS from the original video
+    width  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    fps    = cap.get(cv2.CAP_PROP_FPS)
+    # Define the codec and output file
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # 'XVID' for .avi
+    out = cv2.VideoWriter("video_classification1.mp4", fourcc, fps, (width, height))
+
+    print("VEHICLE DETECTION,CLASSIFICATION AND COUNTING")
+
+    #---------------------------------------------------RETRIEVING VEHICLES-------------------------------------------------------------------------------
+    if (cap.isOpened()== False):
+      print("Error opening video stream or file")
+
+    while(cap.isOpened()):
+        ret,frame=cap.read() 
+        if not ret or frame is None:
+            break
+        frame=cv2.resize(frame,(900,500))
+        for i in cars:
+            i.age_one()
+        fgmask=fgbg.apply(frame)
+
+    #------------------------------------------------------BINARIZATION----------------------------------------------------------------------------
+        if ret==True:
+            ret,imBin=cv2.threshold(fgmask,200,255,cv2.THRESH_BINARY)
+            mask = cv2.morphologyEx(imBin, cv2.MORPH_OPEN, kernalOp) #Opening :E->D
+            mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE, kernalCl) #Closing :D->E
+
+            (contours0,hierarchy)=cv2.findContours(mask,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE) #Contour Extraction
+            for cnt in contours0:
+                area=cv2.contourArea(cnt)
+                #print(area) #Printing the Area of each Object 
+
+                if area>300:
+                    m=cv2.moments(cnt)
+                    #Extracting Centroid Values
+                    cx=int(m['m10']/m['m00'])
+                    cy=int(m['m01']/m['m00'])
+                    x,y,w,h=cv2.boundingRect(cnt) #x,y coordinates,width,height
+
+
+                    new=True
+                    if cy in range(up_limit,down_limit):
+                        for i in cars:
+                            if abs(x - i.getX()) <= w and abs(y - i.getY()) <= h:
+                                new = False
+                                i.updateCoords(cx, cy)
+
+                                if i.going_UP(line_down,line_up)==True:
+                                    cnt_up+=1
+                                    # img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                                    # cv2.imwrite("./detected_vehicles/vehicleUP" + str(cnt_up) + ".png", img[y:y + h - 1, x:x+w])
+
+
+
+                                elif i.going_DOWN(line_down,line_up)==True:
+                                    cnt_down+=1
+                                    # img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+                                    # cv2.imwrite("./detected_vehicles/vehicleDOWN" + str(cnt_down) + ".png", img[y:y + h - 1, x:x+w])
+
+
+                            #     break
+                            if i.getState()=='1':
+                                if i.getDir()=='down'and i.getY()>down_limit:
+                                    i.setDone()
+                                elif i.getDir()=='up'and i.getY()<up_limit:
+                                    i.setDone()
+                            if i.timedOut():
+                                index=cars.index(i)
+                                cars.pop(index)
+                                del i
+
+                        if new==True:
+                            p=vehicle.Car(pid,cx,cy,max_p_age)
+                            carCount+=1
+                            cars.append(p)
+                            pid+1
+                    #cv2.circle(frame, (cx, cy), 20, (0, 0, 255), 2)
+                    # img=cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+
+
+
+            for i in cars:
+                cv2.putText(frame, str(i.getId()), (i.getX(), i.getY()), font, 0.3, (255,255,0), 1, cv2.LINE_AA)
+                if line_down+20<= i.getY() <= line_up-20:
+                   a = (h + (.74*w)- 100)
+
+                   if a >= 0:
+                         cv2.putText(frame, "Truck", (i.getX(), i.getY()), font, 1, (0,255,255), 2, cv2.LINE_AA)
+                         truckCount+=1
+                   else:
+                         cv2.putText(frame, "car", (i.getX(), i.getY()), font, 1, (0,0,255), 2, cv2.LINE_AA)
+                         carCount+=1
+
+
+            # str_up='UP: '+str(cnt_up)
+            # str_down='DOWN: '+str(cnt_down)
+
+            #To display the Lines
+            # frame=cv2.line(frame,(0,line_up),(900,line_up),(255,0,255),3,8) #Magenta
+            # frame=cv2.line(frame,(0,up_limit),(900,up_limit),(0,255,255),3,8) #Cyan
+            # frame=cv2.line(frame,(0,down_limit),(900,down_limit),(255,0,0),3,8) #Yellow
+            # frame = cv2.line(frame, (0, line_down), (900, line_down), (255, 0,0), 3, 8) #Blue
+
+            # #To display the Texts
+            # cv2.putText(frame, str_up, (10, 40), font, 0.5, (0, 0, 255), 2, cv2.LINE_AA)
+            # cv2.putText(frame, str_down, (10, 90), font, 0.5, (255, 0, 0), 2, cv2.LINE_AA)
+
+            cv2.imshow('Frame',frame)
+            # out.write(frame)
+
+            if cv2.waitKey(1)&0xff==ord('q'):
+                break
+
+        else:
+            break
+
+
+    print(carCount)
+    # print(truckCount)
+    cap.release()
+    # out.release()
+
+    # subprocess.run([
+    #     r"C:\ffmpeg\bin\ffmpeg.exe",
+    #     "-i", "output_video1.mp4",
+    #     "-vcodec", "libx264",
+    #     "-acodec", "aac",
+    #     "-strict", "-2",
+    #     "output_video.mp4"
+    # ])
+    subprocess.run([
+        r"C:\ffmpeg\bin\ffmpeg.exe",
+        "-i", "video_classification1.mp4",
+        "-vcodec", "libx264",
+        "-acodec", "aac",
+        "-strict", "-2",
+        "video_classification.mp4"
+    ])
+
+
+    socketio.emit("classify","http://127.0.0.1:5000/classify")
+    # cv2.destroyAllWindows()
 
 
 
@@ -156,30 +334,51 @@ def index():
     return jsonify({"status":"Server is running successfully"})
 
 
-@app.route('/deleteItem', methods=['POST'])
-def deleteFromJson():
-    reqjson = request.get_json()
-    vehicleNo = reqjson['vehicleNo']
-    deleteItem(vehicleNo)
-    return jsonify({'msg': 'ok'})
+# @app.route('/deleteItem', methods=['POST'])
+# def deleteFromJson():
+#     reqjson = request.get_json()
+#     vehicleNo = reqjson['vehicleNo']
+#     deleteItem(vehicleNo)
+#     return jsonify({'msg': 'ok'})
 
 
-@app.route('/pay-challan', methods=['POST'])
-def payChallan():
-    reqjson = request.get_json()
-    vehicleNo = reqjson['vehicleNo']
-    payTheChallan(vehicleNo)
-    return jsonify({'msg': 'ok'})
+# @app.route('/pay-challan', methods=['POST'])
+# def payChallan():
+#     reqjson = request.get_json()
+#     vehicleNo = reqjson['vehicleNo']
+#     payTheChallan(vehicleNo)
+#     return jsonify({'msg': 'ok'})
 
 
 @app.route('/video', methods=['POST'])
 def predictVideo():
     print("got here something")
+    try:
+        os.remove("output_video1.mp4")
+    except:
+        print("File doesnt existed then how delete")
+    try:
+        os.remove("output_video.mp4")
+    except:
+        print("File doesnt existed then how delete")
+    try:
+        os.remove("video_classification.mp4")
+    except:
+        print("File doesnt existed then how delete")
+    try:
+        os.remove("video_classification1.mp4")
+    except:
+        print("File doesnt existed then how delete")
     myfile = request.files['file']
     filename = myfile.filename
     filepath = f"upload/{filename}"
     myfile.save(filepath)
-    readVideo(filepath)
+    videoThread = Thread(target=readVideo,args=(filepath,))
+    videoThread.start()
+    Thread(target=classify,args=(filepath,)).start()
+    # readVideo(filepath)
+    # classify(filepath)
+    videoThread.join()
     os.remove(filepath)
     subprocess.run([
         r"C:\ffmpeg\bin\ffmpeg.exe",
@@ -190,13 +389,16 @@ def predictVideo():
         "output_video.mp4"
     ])
     return jsonify({"Status":"http://127.0.0.1:5000/output"})
-    # return jsonify({'msg': 'ok'})
 
 
 @app.route('/output')
 def serve_video():
     return send_from_directory(".","output_video.mp4")
 
+
+@app.route('/classify')
+def serve_classify():
+    return send_from_directory(".","video_classification.mp4")
 
 
 if __name__ == '__main__':
